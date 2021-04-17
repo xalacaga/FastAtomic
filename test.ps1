87295dad1395f9ece9a7ee22988c3ae779df72fe
xalacaga <33938540+xalacaga@users.noreply.github.com>
2021-04-17 10:03:54 +0200

    Delete test.ps1

--- test.ps1
+++
@@ -1,3 +0,0 @@
-Import-module ImportExcel
-Set-Location -Path "C:\FastAtomic"
-Import-Excel -Path .\matrice.xlsx -WorksheetName 'Matrice Windows' -HeaderName 'Tactic',"Technique #" | Where-Object 'Technique #' -eq T1552.001
